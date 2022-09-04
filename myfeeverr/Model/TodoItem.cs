312892c57b6a47a95daa16fbb4a27c337698e2ad
krishnakumawat8567 <109857266+krishnakumawat8567@users.noreply.github.com>
2022-09-05 01:40:03 +0530

    Init commit....

---
+++ myfeeverr/Model/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace myfeeverr.Model
+{
+    public class TodoItem
+    { public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
